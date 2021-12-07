1fcc2a9b17b49a3901b68043594082721ed59b95
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-12-06 20:06:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
 
 // Routes
-app.use(require('./routes/employees'));
+app.use(require("./routes/employees"));
 
 // Starting the server
-app.listen(app.get('port'), () => {
-  console.log('Server on port', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
 });
